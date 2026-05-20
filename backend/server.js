e68ffa90a30c10c87cc86f3590712d670855447d
Kadir Zeybekoglu <kadrzeybek@gmail.com>
2026-05-20 22:15:00 +0300

    backend part completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import authRouter from './routes/authRoutes.js';
+import modelRouter from './routes/modelRoutes.js'
+import authRouter from './routes/authRoutes.js'
+import instructorRouter from './routes/instructorRoutes.js'
+import studentRouter from './routes/studentRoutes.js'
+import reportRouter from './routes/reportRoutes.js'
 
 const app = express();
 
@@ -16,6 +20,9 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/auth', authRouter)
+app.use('/api/model', modelRouter);
+app.use('/api/instructors', instructorRouter)
+app.use('/api/reports', reportRouter)
 
 app.listen(PORT, () =>{
     console.log('Server is running on Port:',PORT)
